e81230782e88f77a3aa94f31d83127375f4bd72a
r06944010 <kgb@m40.hao>
2019-01-13 22:00:21 +0800

    modify path

--- path.sh
+++ path.sh
@@ -1,6 +1,7 @@
 export KALDI_ROOT=/home/kgb/kaldi-DFSMN/kaldi
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export SRILM=/home/kgb/kaldi-DFSMN/kaldi/tools/srilm
+export PATH=${PATH}:${SRILM}/bin:${SRILM}/bin/i686-m64
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
